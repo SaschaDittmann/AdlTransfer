903bb3aef7532fc95e522bd0055c5c2be86865dc
Sascha Dittmann <Sascha5812@gmx.de>
2016-08-23 17:28:22 +0200

    Initial commit

--- AdlTransfer/Properties/AssemblyInfo.cs
+++ AdlTransfer/Properties/AssemblyInfo.cs
@@ -2,15 +2,20 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AdlTransfer")]
+[assembly: AssemblyDescription("AdlTransfer is designed for high-performance uploading and downloading data to and from Microsoft Azure Data Lake Store.")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sascha Dittmann")]
 [assembly: AssemblyProduct("AdlTransfer")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -20,9 +25,11 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
+//      Minor Version
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
